87190c213976c7b506395d583d26f513d22b16bc
Billzabob <cocolymoo@gmail.com>
2022-09-09 13:16:18 -0600

    Rename table

--- src/db-client.ts
+++ src/db-client.ts
@@ -1,25 +1,26 @@
 import { DynamoDBClient, GetItemCommand, GetItemCommandInput, UpdateItemCommand, UpdateItemCommandInput } from '@aws-sdk/client-dynamodb';
 
 const dbclient = new DynamoDBClient({ region: 'us-west-1' })
+const dailyDataTable = 'DailyData'
 
 export async function getNumCorrectAnswers(day: number) {
   const params: GetItemCommandInput = {
-    TableName: 'num_correct_answers',
+    TableName: dailyDataTable,
     Key: { day: { N: day.toString() } },
-    ProjectionExpression: 'correct_answers',
+    ProjectionExpression: 'CorrectAnswers',
   }
   const data = await dbclient.send(new GetItemCommand(params))
-  return data?.Item?.correct_answers?.N || 0
+  return data?.Item?.CorrectAnswers?.N || 0
 }
 
 export async function incrementCorrectAnswers(day: number) {
   const params: UpdateItemCommandInput = {
-    TableName: 'num_correct_answers',
+    TableName: dailyDataTable,
     Key: { day: { N: day.toString()}},
     ExpressionAttributeValues: { ':val': { N: '1' } },
-    UpdateExpression: 'ADD correct_answers :val',
+    UpdateExpression: 'ADD CorrectAnswers :val',
     ReturnValues: 'UPDATED_NEW'
   }
   const data = await dbclient.send(new UpdateItemCommand(params))
-  return data?.Attributes?.correct_answers?.N || 0
+  return data?.Attributes?.CorrectAnswers?.N || 0
 }
