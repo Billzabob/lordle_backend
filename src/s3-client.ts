c3c989c96b81b08433bfe4cc49a0a1b3b599cb48
Billzabob <cocolymoo@gmail.com>
2022-10-13 07:31:42 -0600

    Revert "Revert "Use .ogg files for sound""
    This reverts commit 138ec7fad67e209b526dc55f3658f172fc7a234a.

--- src/s3-client.ts
+++ src/s3-client.ts
@@ -1,11 +1,11 @@
 import { S3Client, ListObjectsCommand, ListObjectsCommandInput } from "@aws-sdk/client-s3"
 
 const s3sclient = new S3Client({ region: 'us-west-1' })
-const url = 'https://lor-voice-lines.s3.us-west-1.amazonaws.com/' 
+const url = 'https://lor-quotes.s3.us-west-1.amazonaws.com/' 
 
 export async function getVoiceLinesForCard(card: string) {
   const params: ListObjectsCommandInput = {
-    Bucket: 'lor-voice-lines',
+    Bucket: 'lor-quotes',
     Prefix: 'lordle_voices/' + card
   }
 
